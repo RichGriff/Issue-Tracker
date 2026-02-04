0c922a01edb41e1a94bacfeeaf5c48824cf10a69
Richard Griffiths <rich.griffiths89@gmail.com>
2026-02-04 21:32:51 +0000

    task: set cors middleware

--- main.py
+++ main.py
@@ -3,11 +3,19 @@ from fastapi import FastAPI
 import app.database.models as models
 from app.database.config import engine
 from app.middleware.timing import timing_middleware
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes.issues import router as issues_router
 
 app = FastAPI()
 
 app.middleware("http")(timing_middleware)
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=["http://localhost:3000"],
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
+)
 
 models.Base.metadata.create_all(bind=engine)
 
